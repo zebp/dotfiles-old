0dd9247acd04bf95991f13bcdf4cf84fcb895e8f
Zeb Piasecki <zeb@zebulon.dev>
2021-06-19 12:59:01 -0400

    make install script non-interactive

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ printf "\n🚀 Installing fzf\n"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 # Install fzf
-~/.fzf/install
+yes Y | ~/.fzf/install
 
 printf "\n🚀 Installing fast node manager\n"
 
@@ -19,17 +19,11 @@ curl -fsSL https://fnm.vercel.app/install | bash
 printf "\n🚀 Installing oh-my-zsh\n"
 
 # Install oh-my-zsh
-RUNZSH="no" sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+CHSH="no" RUNZSH="no" sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 printf "\n🚀 Linking dotfiles\n"
 
 ln -sf "$(pwd)/zsh/.zshrc" "${HOME}/.zshrc"
 ln -sf "$(pwd)/zsh/zeb.zsh-theme" "${HOME}/.oh-my-zsh/custom/themes/"
 
-printf "\n🚀 Setting zsh to default shell\n"
-chsh -s /bin/zsh
-
 printf "\n🚀 Done!\n"
-
-
-zsh
